19844a911111f365b363ab32c1d1ac4c04bdb2b7
AbdelrahmanEssamA <abdelrahmanea97@gmail.com>
2021-03-02 03:20:41 +0200

    added file system added more pages, added more variables to the product model and fixed the styling

--- routes/admin.js
+++ routes/admin.js
@@ -2,14 +2,12 @@ const path = require('path');
 
 const express = require('express');
 
-const productsController = require('../Controllers/products');
+const productsController = require('../Controllers/admin');
 
 const router = express.Router();
 
-// /admin/add-product => GET
 router.get('/add-product', productsController.getAddProduct);
-
-// /admin/add-product => POST
+router.get('/products', productsController.getProducts);
 router.post('/add-product', productsController.postAddProduct);
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -2,9 +2,14 @@ const path = require('path');
 
 const express = require('express');
 
-const productsController = require('../Controllers/products');
+const shopController = require('../Controllers/shop');
 
 const router = express.Router();
-router.get('/', productsController.getProducts);
+router.get('/', shopController.getIndex);
+
+router.get('/product-list', shopController.getProducts);
+router.get('/cart', shopController.getCart);
+router.get('/checkout', shopController.getCheckout);
+router.get('/orders', shopController.getOrders);
 
 module.exports = router;
